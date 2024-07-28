f2e76e1a7987110859f8252ce705534ad6fbd488
idris <idris.somoye@gmail.com>
2024-07-28 15:17:55 -0400

    flatten verif hierchy

--- src/verif/env/pcie_env_config.py
+++
@@ -1,9 +0,0 @@
-from pyuvm import *
-
-class pcie_env_config(uvm_component):
-    def __init__(self, name = "pcie_env_config"):
-        super().__init__(name)
-        self.has_scoreboard       =  1  # type: bit  
-        self.has_coverage_monitor =  1  # type: bit  
-        # self.lpif_agent_config_h  = None  # type: lpif_agent_config  
-        self.pipe_agent_config_h  = None  # type: pipe_agent_config  
